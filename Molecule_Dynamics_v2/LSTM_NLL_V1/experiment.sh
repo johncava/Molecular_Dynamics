fe7feebe067e9f84a39c3a1ecd6e9bf331e41033
NickDST <learningnickk@gmail.com>
2021-09-10 00:01:22 -0700

    NLL models

---
+++ Molecule_Dynamics_v2/LSTM_NLL_V1/experiment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+ 
+#SBATCH -N 1  # number of nodes
+#SBATCH -n 8  # number of "tasks" (default: allocates 1 core per task)
+#SBATCH -t 0-05:00:00   # time in d-hh:mm:ss
+#SBATCH -p gpu       # partition
+#SBATCH --gres=gpu:1  
+#SBATCH -q wildfire       # QOS
+#SBATCH -o slurm.%j.out # file to save job's STDOUT (%j = JobId)
+#SBATCH -e slurm.%j.err # file to save job's STDERR (%j = JobId)
+#SBATCH --mail-type=ALL # Send an e-mail when a job starts, stops, or fails
+#SBATCH --mail-user=nichola2@asu.edu # Mail-to address
+
+# source activate ~/.conda/envs/pytorch-geometric
+conda activate nichola2_pt
+python LSTMNLL_Train.py
+conda deactivate
