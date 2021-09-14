85cd93a02cfad0c237c6a97a2606399c6556eebb
johncava <jcava@asu.edu>
2021-09-14 00:11:40 -0700

    seq2seq

---
+++ Molecule_Dynamics_v3/Seq2Seq_V1/seq2seq.py
@@ -0,0 +1,256 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn 
+import torch.nn.functional as F
+
+import torch_geometric.transforms as T
+from torch_geometric.data import Data 
+from torch_geometric.nn import GATConv
+
+
+###
+# IMPORTANT VARIABLES
+###
+
+number_of_particles = 40 
+input_size = 3
+hidden_size = 128
+history_size = 15
+lead_time = 2
+M = 5
+num_layers = 1
+
+##
+# Read Dataset 
+##
+import glob
+import numpy as np
+
+files = glob.glob('./../../All_ML_Training_Data/210905_SMD_decaalanine/SMD/output/processed_orient/*.npy')
+
+dataset = []
+
+for file_ in files:
+    X_positions = np.load(file_)
+
+    #Pick the good region [5K-10K]
+    X = X_positions[5000:10001,:,:]
+
+    # Sample down the amount of sequenced frames from 20K to 2K
+    X = X[::10]
+    #print(X.shape)
+
+    dataset.append((X[:-1,:,:], X[1:,:,:]))
+
+# Shuffle the dataset
+import random
+random.seed(123)
+np.random.seed(123)
+torch.manual_seed(123)
+
+split = int(len(dataset)*0.8)
+training_dataset = dataset[:split]
+testing_dataset = dataset[split:]
+random.shuffle(training_dataset)
+
+# Dataset size
+print(len(training_dataset))
+
+##
+# LSTM Definition
+##
+
+class Encoder(nn.Module):
+
+    def __init__(self, input_size, hidden_size):
+        super(Encoder, self).__init__()
+        self.lstm = nn.LSTM(input_size,hidden_size,num_layers=num_layers).cuda()
+        self.h0 = torch.randn((num_layers, number_of_particles, hidden_size)).cuda()
+        self.c0 = torch.randn((num_layers, number_of_particles, hidden_size)).cuda()
+
+    def reinitalize(self):
+        self.h0 = torch.randn((num_layers, number_of_particles, hidden_size)).cuda()
+        self.c0 = torch.randn((num_layers, number_of_particles, hidden_size)).cuda()
+
+    def forward(self,x):
+        x , (self.h0,self.c0) = self.lstm(x,(self.h0, self.c0))
+        return x, (self.h0, self.c0)
+
+class Decoder(nn.Module):
+
+    def __init__(self, input_size, hidden_size):
+        super(Decoder, self).__init__()
+        self.lstm = nn.LSTM(input_size,hidden_size,num_layers=num_layers).cuda()
+        self.mlp = nn.Linear(hidden_size,3).cuda()
+
+    def forward(self,x,h0,c0):
+        x , (h0,c0) = self.lstm(x,(h0,c0))
+        x = self.mlp(x)
+        return x, (h0,c0)
+
+##
+# LSTM Initializations
+##
+
+encoder = Encoder(input_size, hidden_size)
+decoder = Decoder(input_size, hidden_size)
+
+##
+# Optimization
+##
+import torch.optim as optim
+learning_rate=1e-3
+optimizer = optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=learning_rate)
+
+##
+# Run Training
+##
+
+max_epochs = 5
+
+epoch_loss = []
+
+import time
+start = time.time()
+for epoch in range(max_epochs):
+
+    training_loss = []
+    for data in training_dataset:
+        #print(data[0].shape,data[1].shape)
+        x = torch.tensor(data[0]).float().cuda()
+        #x_final = x[-1,:,:3]
+        y = torch.tensor(data[1]).float().cuda()
+        # Encoder
+        encoder.reinitalize()
+        output,(h0,c0) = encoder(x)
+        # Decoder
+        predictions = []
+        for i in range(output.size()[0]):
+            noise_input = torch.randn(1,40,3).cuda()
+            output, (h0,c0) = decoder(noise_input, h0,c0)
+            predictions.append(output.squeeze(0))
+        predictions = torch.stack(predictions)
+        # Loss computation
+        optimizer.zero_grad()
+        loss = F.mse_loss(predictions, y)
+        training_loss.append(loss.item())
+        loss.backward()
+        optimizer.step()
+        break
+    epoch_loss.append(np.mean(training_loss))
+    print('Epoch ' + str(epoch) + ' Loss: ' + str(epoch_loss[-1]))
+end = time.time()
+print('Done in ' + str(end-start) + 's')
+
+
+##
+# Run Testing Non-AutoRegressively
+##
+
+X_positions = np.load('/home/jcava/10_deca_alanine/99/backbone.npy') #/ 17.0
+
+# Reshape X_angles to get every 10th frame (200000, number of particles, 2) => (20000, number of particles, 2)
+
+#Pick the good region [5K-10K]
+X = X_positions[5000:10001,:,:]
+X = X[::10]
+
+testing_dataset = [X]
+
+predictions = None
+for data in testing_dataset:
+    # LSTM Encoder
+    x = torch.tensor(data).float().cuda()
+    # Encoder
+    encoder.reinitalize()
+    output,(h0,c0) = encoder(x)
+    # Decoder
+    predictions = []
+    for i in range(output.size()[0]):
+        noise_input = torch.randn(1,40,3).cuda()
+        output, (h0,c0) = decoder(noise_input, h0,c0)
+        predictions.append(output.squeeze(0))
+    predictions = torch.stack(predictions)
+
+#predictions[:,:,:2] =  predictions[:,:,:2] * 7.0
+#predictions[:,:,2:3] = predictions[:,:,2:3] * 10.0
+# Save predictions
+predictions = predictions.detach().cpu().numpy()
+np.save("predictions.npy", predictions)
+
+# Save predictions into VMD format
+frame_num = predictions.shape[0]
+
+nAtoms = "40"
+outName = "predictions.xyz"
+with open(outName, "w") as outputfile:
+    for frame_idx in range(frame_num):
+        
+        frame = predictions[frame_idx]
+        outputfile.write(str(nAtoms) + "\n")
+        outputfile.write(" generated by JK\n")
+
+        atomType = "CA"
+        for i in range(40):
+            line = str(frame[i][0]) + " " + str(frame[i][1]) + " " + str(frame[i][2]) + " "
+            line += "\n"
+            outputfile.write("  " + atomType + "\t" + line)
+
+print("=> Finished Generation <=")
+
+##
+# Run Testing Auto-Regressively
+##
+
+X_positions = np.load('/home/jcava/10_deca_alanine/99/backbone.npy') #/ 17.0
+
+# Reshape X_angles to get every 10th frame (200000, number of particles, 2) => (20000, number of particles, 2)
+
+#Pick the good region [5K-10K]
+X = X_positions[5000:10001,:,:]
+X = X[::10]
+
+testing_dataset = [X]
+
+predictions = None
+with torch.no_grad():
+
+    x = torch.tensor(testing_dataset[0]).float().cuda()
+    # Encoder
+    encoder.reinitalize()
+    output,(h0,c0) = encoder(x)
+    # Decoder
+    predictions = []
+    decoder_input = torch.randn(1,40,3).cuda()
+    for i in range(output.size()[0]):
+        output, (h0,c0) = decoder(decoder_input, h0,c0)
+        predictions.append(output.squeeze(0))
+        decoder_input = output
+
+    predictions = torch.stack(predictions).squeeze(1)
+    predictions = predictions.cpu().detach().numpy()
+print(predictions.shape)
+
+np.save("predictions-auto.npy", predictions)
+
+# Save predictions into VMD format
+frame_num = predictions.shape[0]
+
+nAtoms = "40"
+outName = "predictions-auto.xyz"
+with open(outName, "w") as outputfile:
+    for frame_idx in range(frame_num):
+        
+        frame = predictions[frame_idx]
+        outputfile.write(str(nAtoms) + "\n")
+        outputfile.write(" generated by JK\n")
+
+        atomType = "CA"
+        for i in range(40):
+            line = str(frame[i][0]) + " " + str(frame[i][1]) + " " + str(frame[i][2]) + " "
+            line += "\n"
+            outputfile.write("  " + atomType + "\t" + line)
+
+print("=> Finished Generation <=")
