ed490fb249cf43c3a9b09ad61959031a27e9f58f
johncava <jcava@asu.edu>
2021-12-15 14:43:33 -0700

    V2.1.5 Added the ability to change smoothing params based on epoch

---
+++ cGAN/cGAN_D_pos-2.1.5/loadall.tcl
@@ -0,0 +1,36 @@
+mol default style Licorice
+
+set sys ./
+set sysname "pretrain_generated"
+mol new $sys/da.psf
+mol addfile $sys/pretrain_generated.xyz waitfor -1
+set myid [molinfo top]
+# Rename
+mol rename $myid $sysname
+mol modmaterial 0 $myid Opaque
+mol modcolor 0 $myid Name
+mol modselect 0 $myid all
+ 
+## Genergated by GAN
+set sysname "cGAN_generated"
+mol new $sys/da.psf
+mol addfile $sys/cGAN_generated.xyz waitfor -1
+set myid [molinfo top]
+# Rename
+mol rename $myid $sysname
+mol modmaterial 0 $myid Opaque
+mol modcolor 0 $myid Name
+mol modselect 0 $myid all
+
+# Pretraining Training data set
+set sysname "pretrain_train_data"
+mol new $sys/da.psf
+mol addfile $sys/pretrain_training_data.xyz waitfor -1
+set myid [molinfo top]
+# Rename
+mol rename $myid $sysname
+mol modmaterial 0 $myid Opaque
+mol modcolor 0 $myid Name
+mol modselect 0 $myid all
+
+
